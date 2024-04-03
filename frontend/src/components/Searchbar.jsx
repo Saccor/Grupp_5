9b1144e07c4a6d3083fec0d63bfd079e5e7f3bc6
1saifu <saifukhan@hotmail.se>
2024-04-03 17:06:50 +0200

    searchbar completed

--- frontend/src/components/Searchbar.jsx
+++ frontend/src/components/Searchbar.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { useState } from "react";
 
-const Searchbar = () => {
-  return <div></div>;
+const Searchbar = ({ products, setFilteredProducts }) => {
+
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleChange = (event) => {
+    const searchTerm = event.target.value.toLowerCase();
+    setSearchTerm(searchTerm);
+    const filtered = products.filter((product) => 
+    product.name.toLowerCase().includes(searchTerm)
+    )
+    setFilteredProducts(filtered)
+  }
+
+  return <div>
+    <input 
+    type="text"
+    placeholder="Search for products.."
+    value={searchTerm} 
+    onChange={handleChange}
+    className="search-bar"
+    />
+  </div>;
 };
 
 export default Searchbar;
