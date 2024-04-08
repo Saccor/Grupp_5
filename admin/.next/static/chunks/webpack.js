5ab3bc92a66896ae8f6a5fa68b6c46c9073fe2bd
Adamk1979 <adamkhalil040505@gmail.com>
2024-04-08 21:00:48 +0200

    fix bug

--- admin/.next/static/chunks/webpack.js
+++ admin/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "33c3995b7331b670"; }
+/******/ 		__webpack_require__.h = function() { return "203dec5a56dcc084"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
