da577a3312cacbf891ec969fed1bb2fc51ac39bd
Adamk1979 <adamkhalil040505@gmail.com>
2024-04-08 16:45:45 +0200

    Added 'react' and 'react-dom' to dependencies to fix build errors

--- admin/.next/static/chunks/webpack.js
+++ admin/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6a7d21fd19de2e7e"; }
+/******/ 		__webpack_require__.h = function() { return "33c3995b7331b670"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
