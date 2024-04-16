41bb089afd590141af556c345ff7cc78ddefa74b
Adamk1979 <adamkhalil040505@gmail.com>
2024-04-16 18:39:01 +0200

    new productform

--- admin/models/Product.js
+++ admin/models/Product.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-import mongoose, {model, Schema, models} from "mongoose";
-
-const ProductSchema = new Schema({
-  title: {type:String, required:true},
-  description: String,
-  price: {type: Number, required: true},
-  images: [{type:String}],
-  category: {type:mongoose.Types.ObjectId, ref:'Category'},
-  properties: {type:Object},
-}, {
-  timestamps: true,
-});
-
-export const Product = models.Product || model('Product', ProductSchema);
-=======
 import mongoose, { model, Schema, models } from "mongoose";
 
 const ProductSchema = new Schema({
@@ -28,4 +12,3 @@ const ProductSchema = new Schema({
 });
 
 export const Product = models.Product || model('Product', ProductSchema);
->>>>>>> adamtest
