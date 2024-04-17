379a5c7d3a59e43341132e5c9c57df236cb2f58c
1saifu <saifukhan@hotmail.se>
2024-04-17 16:44:19 +0200

    api

--- frontend/src/services/apiServices.js
+++ frontend/src/services/apiServices.js
@@ -1,8 +1,15 @@
+// import axios from "axios";
+
+// export const fetchProducts = async () => {
+//   const response = await axios.get(
+//     `${process.env.REACT_APP_BACKEND_URL}/api/multi-collection-products`
+//   );
+//   return response.data;
+// };
+
 import axios from "axios";
 
 export const fetchProducts = async () => {
-  const response = await axios.get(
-    `${process.env.REACT_APP_BACKEND_URL}/api/multi-collection-products`
-  );
+  const response = await axios.get("http://localhost:3001/api/multi-collection-products");
   return response.data;
 };
