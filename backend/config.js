55cebf6a0889e41c67c2ec568b4cbbfc76f6cce6
Saccor <sacco.rezais@gmail.com>
2024-04-17 12:49:00 +0200

    fixed server.js/app.js and config.js

--- backend/config.js
+++ backend/config.js
@@ -5,3 +5,6 @@ dotenv.config();
 export const MONGO_URI = process.env.MONGO_DATA_API_URL;
 export const SESSION_SECRET = process.env.SESSION_SECRET;
 export const PORT = process.env.PORT || 3000;
+
+// In your config.js, add the following
+export const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY;
