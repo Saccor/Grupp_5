1b8f86b935e847b3483e7def1c3cf2d0b1f49b9b
Adamk1979 <adamkhalil040505@gmail.com>
2024-04-19 02:27:04 +0200

    orders test

--- admin/models/Order.js
+++ admin/models/Order.js
@@ -1,16 +1,37 @@
-import {model, models, Schema} from "mongoose";
+import mongoose from "mongoose";
 
-const OrderSchema = new Schema({
-  line_items:Object,
-  name:String,
-  email:String,
-  city:String,
-  postalCode:String,
-  streetAddress:String,
-  country:String,
-  paid:Boolean,
-}, {
-  timestamps: true,
+const orderItemSchema = new mongoose.Schema({
+  product: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
+  quantity: { type: Number, required: true },
 });
 
-export const Order = models?.Order || model('Order', OrderSchema);
+const customerSchema = new mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true },
+  address: { type: String, required: true },
+});
+
+const paymentDetailsSchema = new mongoose.Schema({
+  cardNumber: { type: String, required: true, select: false },
+  cvv: { type: String, required: true, select: false },
+  expiryDate: { type: String, required: true },
+});
+
+const orderSchema = new mongoose.Schema({
+  orderItems: [orderItemSchema],
+  customer: customerSchema,
+  paymentDetails: paymentDetailsSchema,
+  paymentStatus: {
+    type: String,
+    default: "Unpaid",
+    enum: ["Paid", "Unpaid"]
+  },
+  total: { type: Number, required: true },
+  totalIncludingVAT: { type: Number, required: true },
+  paid: { type: Boolean, default: false },
+}, { timestamps: true });
+
+const Order = mongoose.model("Order", orderSchema);
+
+export default Order;
