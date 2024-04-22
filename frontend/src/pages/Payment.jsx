cc93e1d716ed0652045d488fae23f9867c2bbe5a
Miriam-arch <124446711+Miriam-arch@users.noreply.github.com>
2024-04-23 00:36:34 +0200

    Fixed confirmation message after payment.

--- frontend/src/pages/Payment.jsx
+++ frontend/src/pages/Payment.jsx
@@ -5,6 +5,7 @@ import axios from "axios";
 const Payment = () => {
   const { state } = useLocation();
   const navigate = useNavigate();
+  const [isPaymentSuccessful, setIsPaymentSuccessful] = useState(false);
 
   const [paymentDetails, setPaymentDetails] = useState({
     cardNumber: "",
@@ -33,6 +34,7 @@ const Payment = () => {
     e.preventDefault();
 
     try {
+      console.log("State:", state);
       const orderItems = state.products
         ? state.products.map((product) => ({
             product: product,
@@ -45,7 +47,11 @@ const Payment = () => {
         0
       );
 
-      const totalIncludingVAT = total * 1.12;
+      const totalIncludingVAT = total * 1.12; // Assuming VAT is 12%
+
+      console.log("Order Items:", orderItems);
+      console.log("Total:", total);
+      console.log("Total Including VAT:", totalIncludingVAT);
 
       const orderData = {
         orderItems: orderItems,
@@ -56,55 +62,114 @@ const Payment = () => {
         totalIncludingVAT: totalIncludingVAT,
       };
 
+      console.log("Order Data:", orderData);
+
       const response = await axios.post(
         `${process.env.REACT_APP_BACKEND_URL}/api/orders`,
         orderData
       );
 
-      navigate("/order-confirmation");
+      console.log("Order created:", response.data);
+      setIsPaymentSuccessful(true); // Set the payment success state to true
+      // navigate("/order-confirmation");
     } catch (error) {
       console.error("Error creating order:", error);
     }
   };
 
+  if (isPaymentSuccessful) {
+    return (
+      <div className="confirmation-message">
+        <h2>Tack för ditt köp!</h2>
+        <h4>Vi skickar en orderbekräftelse till din e-post med ditt kvitto.</h4>
+        <h4>Din beställning kommer att levereras inom 3-5 arbetsdagar.</h4>
+      </div>
+    );
+  }
+
   return (
-    <div className="payment-form" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
-      <form onSubmit={handleSubmit} style={{ width: '100%', maxWidth: '500px' }}>
+    <div className="payment-form">
+      <form onSubmit={handleSubmit}>
         <div className="payment-section">
           <h2>Kunduppgifter</h2>
           <label>
             Förnamn:
-            <input type="text" name="firstName" value={customerDetails.firstName} onChange={handleInputChange} required />
+            <input
+              type="text"
+              name="firstName"
+              value={customerDetails.firstName}
+              onChange={handleInputChange}
+              required
+            />
           </label>
           <label>
             Efternamn:
-            <input type="text" name="lastName" value={customerDetails.lastName} onChange={handleInputChange} required />
+            <input
+              type="text"
+              name="lastName"
+              value={customerDetails.lastName}
+              onChange={handleInputChange}
+              required
+            />
           </label>
           <label>
             Email:
-            <input type="email" name="email" value={customerDetails.email} onChange={handleInputChange} required />
+            <input
+              type="email"
+              name="email"
+              value={customerDetails.email}
+              onChange={handleInputChange}
+              required
+            />
           </label>
           <label>
             Adress:
-            <input type="text" name="address" value={customerDetails.address} onChange={handleInputChange} required />
+            <input
+              type="text"
+              name="address"
+              value={customerDetails.address}
+              onChange={handleInputChange}
+              required
+            />
           </label>
         </div>
+
         <div className="payment-section">
           <h2>Betalningsuppgifter</h2>
+
           <label>
             Kortnummer:
-            <input type="text" name="cardNumber" value={paymentDetails.cardNumber} onChange={handlePaymentInputChange} required />
+            <input
+              type="text"
+              name="cardNumber"
+              value={paymentDetails.cardNumber}
+              onChange={handlePaymentInputChange}
+              required
+            />
           </label>
           <label>
             CVV:
-            <input type="text" name="cvv" value={paymentDetails.cvv} onChange={handlePaymentInputChange} required />
+            <input
+              type="text"
+              name="cvv"
+              value={paymentDetails.cvv}
+              onChange={handlePaymentInputChange}
+              required
+            />
           </label>
           <label>
-            Löper ut:
-            <input type="text" name="expiryDate" value={paymentDetails.expiryDate} onChange={handlePaymentInputChange} required />
+            Giltighetstid:
+            <input
+              type="text"
+              name="expiryDate"
+              value={paymentDetails.expiryDate}
+              onChange={handlePaymentInputChange}
+              required
+            />
           </label>
         </div>
-        <button type="submit" style={{ width: '100%', padding: '8px 14px', backgroundColor: '##e60000', color: 'white', border: 'none', borderRadius: '50px', cursor: 'pointer', marginTop: '20px' }}>Bekräfta köp</button>
+
+        <button type="submit">Bekräfta köp</button>
       </form>
     </div>
   );
