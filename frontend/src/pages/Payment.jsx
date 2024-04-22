05fd57769360a6c12f60c92d3452afe7d66c67c4
Saccor <sacco.rezais@gmail.com>
2024-04-22 23:28:30 +0200

    fixed submit button

--- frontend/src/pages/Payment.jsx
+++ frontend/src/pages/Payment.jsx
@@ -33,7 +33,6 @@ const Payment = () => {
     e.preventDefault();
 
     try {
-      console.log("State:", state);
       const orderItems = state.products
         ? state.products.map((product) => ({
             product: product,
@@ -46,11 +45,7 @@ const Payment = () => {
         0
       );
 
-      const totalIncludingVAT = total * 1.12; // Assuming VAT is 12%
-
-      console.log("Order Items:", orderItems);
-      console.log("Total:", total);
-      console.log("Total Including VAT:", totalIncludingVAT);
+      const totalIncludingVAT = total * 1.12;
 
       const orderData = {
         orderItems: orderItems,
@@ -61,13 +56,10 @@ const Payment = () => {
         totalIncludingVAT: totalIncludingVAT,
       };
 
-      console.log("Order Data:", orderData);
-
       const response = await axios.post(
         `${process.env.REACT_APP_BACKEND_URL}/api/orders`,
         orderData
       );
-      console.log("Order created:", response.data);
 
       navigate("/order-confirmation");
     } catch (error) {
@@ -76,88 +68,43 @@ const Payment = () => {
   };
 
   return (
-    <div className="payment-form">
-      <form onSubmit={handleSubmit}>
+    <div className="payment-form" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+      <form onSubmit={handleSubmit} style={{ width: '100%', maxWidth: '500px' }}>
         <div className="payment-section">
           <h2>Kunduppgifter</h2>
           <label>
             Förnamn:
-            <input
-              type="text"
-              name="firstName"
-              value={customerDetails.firstName}
-              onChange={handleInputChange}
-              required
-            />
+            <input type="text" name="firstName" value={customerDetails.firstName} onChange={handleInputChange} required />
           </label>
           <label>
             Efternamn:
-            <input
-              type="text"
-              name="lastName"
-              value={customerDetails.lastName}
-              onChange={handleInputChange}
-              required
-            />
+            <input type="text" name="lastName" value={customerDetails.lastName} onChange={handleInputChange} required />
           </label>
           <label>
             Email:
-            <input
-              type="email"
-              name="email"
-              value={customerDetails.email}
-              onChange={handleInputChange}
-              required
-            />
+            <input type="email" name="email" value={customerDetails.email} onChange={handleInputChange} required />
           </label>
           <label>
             Adress:
-            <input
-              type="text"
-              name="address"
-              value={customerDetails.address}
-              onChange={handleInputChange}
-              required
-            />
+            <input type="text" name="address" value={customerDetails.address} onChange={handleInputChange} required />
           </label>
         </div>
-
         <div className="payment-section">
           <h2>Betalningsuppgifter</h2>
-
           <label>
             Kortnummer:
-            <input
-              type="text"
-              name="cardNumber"
-              value={paymentDetails.cardNumber}
-              onChange={handlePaymentInputChange}
-              required
-            />
+            <input type="text" name="cardNumber" value={paymentDetails.cardNumber} onChange={handlePaymentInputChange} required />
           </label>
           <label>
             CVV:
-            <input
-              type="text"
-              name="cvv"
-              value={paymentDetails.cvv}
-              onChange={handlePaymentInputChange}
-              required
-            />
+            <input type="text" name="cvv" value={paymentDetails.cvv} onChange={handlePaymentInputChange} required />
           </label>
           <label>
             Löper ut:
-            <input
-              type="text"
-              name="expiryDate"
-              value={paymentDetails.expiryDate}
-              onChange={handlePaymentInputChange}
-              required
-            />
+            <input type="text" name="expiryDate" value={paymentDetails.expiryDate} onChange={handlePaymentInputChange} required />
           </label>
         </div>
-
-        <button type="submit">Bekräfta köp</button>
+        <button type="submit" style={{ width: '100%', padding: '8px 14px', backgroundColor: '##e60000', color: 'white', border: 'none', borderRadius: '50px', cursor: 'pointer', marginTop: '20px' }}>Bekräfta köp</button>
       </form>
     </div>
   );
