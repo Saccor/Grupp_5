e3e7234faac480828f360070b522752e28fb12f7
Miriam-arch <124446711+Miriam-arch@users.noreply.github.com>
2024-04-24 18:37:07 +0200

    fixed new header.

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../styles.css";
 
+const images = [
+  "/image1.webp",
+  "/image2.png",
+  "/image3.png",
+  "/image4.png",
+  "/image5.webp",
+  "/image6.webp",
+  "/image7.webp",
+];
+
 const Header = () => {
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setIndex((prevIndex) =>
+        prevIndex === images.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 3000);
+    return () => clearInterval(timer);
+  }, []);
+
   return (
-    <header className="header">
-      <h1>Hakim Livs</h1>
-    </header>
+    <div className="container">
+      <header className="header">
+        <img
+          src={images[index]}
+          alt={`Slide ${index + 1}`}
+          className="header-image"
+        />
+      </header>
+    </div>
   );
 };
 
